519579b11624b27a0260ef35cd10af831227b720
TelloWick <fabriciovilca_2001@hotmail.com>
2026-06-05 17:51:19 -0500

    Modificacion de archivo AndroidManifest.xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,20 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
 }
+secrets {
+    // Optionally specify a different file name containing your secrets.
+    // The plugin defaults to "local.properties"
+    propertiesFileName = "secrets.properties"
+
+    // A properties file containing default secret values. This file can be
+    // checked in version control.
+    defaultPropertiesFileName = "local.defaults.properties"
+
+    // Configure which keys should be ignored by the plugin by providing regular expressions.
+    // "sdk.dir" is ignored by default.
+    ignoreList.add("keyToIgnore") // Ignore the key "keyToIgnore"
+    ignoreList.add("sdk.*")       // Ignore all keys matching the regexp "sdk.*"
+}
 
 android {
     namespace = "com.example.lab12"
