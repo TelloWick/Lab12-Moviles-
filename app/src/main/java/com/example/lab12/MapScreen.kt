452a802809ac8cb705491f84c31a31d80739361b
TelloWick <fabriciovilca_2001@hotmail.com>
2026-06-07 09:37:36 -0500

    Modificar el archivo MapScreen cambiando a tipo de mapa TERRAIN

--- app/src/main/java/com/example/lab12/MapScreen.kt
+++ app/src/main/java/com/example/lab12/MapScreen.kt
@@ -73,7 +73,7 @@ fun MapScreen() {
             cameraPositionState = cameraPositionState,
             properties = MapProperties(
                 mapType =
-                    MapType.SATELLITE
+                    MapType.TERRAIN
             ),
         ){
 
